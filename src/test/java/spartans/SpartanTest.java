9f7dd566f470d61d5ad1f3200d6fe4df5605e907
Akbar <Akbar@cybertekschool.com>
2020-12-03 14:31:59 -0500

    Added @BeforeAll and @AfterAll to set up and teardown

--- src/test/java/spartans/SpartanTest.java
+++ src/test/java/spartans/SpartanTest.java
@@ -1,12 +1,26 @@
 package spartans;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.*;
 
 public class SpartanTest {
 
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "http://100.26.130.128:8000" ;
+        RestAssured.basePath = "/api" ;
+    }
+
+
     @Test
     public void test1(){
         Assertions.assertEquals(7, 4+3);
+    }
+
+
+    @AfterAll
+    public static void tearDown(){
+        RestAssured.reset();
     }
 
 
